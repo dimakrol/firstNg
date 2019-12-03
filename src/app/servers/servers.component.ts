9546fa1071b3415c95624d1a01eb38759225845b
dima_krol <dmytro.krol@carerix.com>
2019-12-03 16:21:50 +0200

    Initial commit.

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  statusSome: string = 'no server created';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
+
+  ngOnInit() {
+  }
+  onCreateServer() {
+    this.statusSome = 'Server was created';
+  }
+}
